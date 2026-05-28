b0e03fe64c5b591cc89c49730a04fa619b0ff200
Władysław Farat <wfarat@gmail.com>
2026-05-28 21:37:54 +0200

    Added better hud;

--- cannon.gd
+++ cannon.gd
@@ -1,7 +1,14 @@
 extends AnimatedSprite2D
-
+# Arrays to hold the data for each tier!
+# Index 0 is Tier 1, Index 1 is Tier 2, etc.
+@export var tier_animations: Array[SpriteFrames] 
+@export var tier_fire_rates: Array[float] = [0.5, 0.35, 0.2] 
+@export var tier_damage: Array[int] = [20, 30, 45]
+@export var tier_price: Array[int] = [300, 600, 900]
+var current_tier: int = 0 # Starts at 0 (Tier 1)
 @export var bullet_scene: PackedScene 
 @export var fire_rate: float = 0.5 
+@export var damage: int = 20
 
 var current_cooldown: float = 0.0
 
@@ -26,9 +33,23 @@ func shoot():
 	var spawned_bullet = bullet_scene.instantiate()
 	spawned_bullet.global_position = muzzle.global_position
 	spawned_bullet.global_rotation = global_rotation
-	
+	spawned_bullet.damage = damage
 	get_tree().current_scene.add_child(spawned_bullet)
 	
 	play("shoot")
 	await animation_finished
 	play("idle")
+func upgrade_tier():
+	# Check if we haven't reached the max level yet
+	if current_tier < tier_animations.size() - 1:
+		current_tier += 1
+		
+		# 1. Swap the visual animations!
+		sprite_frames = tier_animations[current_tier]
+		fire_rate = tier_fire_rates[current_tier]
+		
+func next_tier_price():
+	if current_tier < tier_animations.size() - 1:
+		return tier_price[current_tier+1]
+	else:
+		return 0
