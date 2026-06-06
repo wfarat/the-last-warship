455ed4d4d49492c2bbfbbef1d8c711580041c71e
PJATKMaciekM <s32006@pjwstk.edu.pl>
2026-06-07 00:02:08 +0200

    port tp cooldown

--- misc/teleport_zone.gd
+++ misc/teleport_zone.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
 @export var dock_target: Marker2D 
+@export var cooldown_time: float = 10.0
+var can_teleport: bool = true
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and can_teleport:
+		can_teleport = false
 		body.global_position = dock_target.global_position
 		body.global_rotation = dock_target.global_rotation
 		body.velocity = Vector2.ZERO 
+		await get_tree().create_timer(cooldown_time).timeout
+		can_teleport = true
